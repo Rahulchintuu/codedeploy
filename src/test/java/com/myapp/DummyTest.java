924cd4befdc624e0007df985ed1ba84ac5fb1f34
Rahulchintuu <rahulteetla01@gmail.com>
2025-10-12 16:08:59 +0530

    Update DummyTest.java

--- src/test/java/com/myapp/DummyTest.java
+++ src/test/java/com/myapp/DummyTest.java
@@ -1,7 +1,7 @@
 package com.myapp;
 
-import org.junit.Test;
-import static org.junit.Assert.assertTrue;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class DummyTest {
     @Test
