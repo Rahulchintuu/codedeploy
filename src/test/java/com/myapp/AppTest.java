ee632ce7b40bcaacb86de01081c207c335740a3a
Rahulchintuu <rahulteetla01@gmail.com>
2025-10-12 19:58:54 +0530

    Create AppTest.java

---
+++ src/test/java/com/myapp/AppTest.java
@@ -0,0 +1,12 @@
+package com.myapp;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class AppTest {
+
+    @Test
+    void sampleTest() {
+        assertTrue(true, "This test always passes");
+    }
+}
